18891ab154f87d1a106d288d8534237a8346c5f9
Faruk Onar <farukoonar@gmail.com>
2021-02-06 11:28:12 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carService = new ICarManager(new InMemoryCarDal());
+
+            Car car1 = new Car { Id = 6, BrandId = 2, ColorId = 5, DailyPrice = 130, ModelYear = 2020, Description = "Otomatik Hibrit" };
+            carService.Add(car1);
+
+            foreach (var car in carService.GetAll())
+            {
+                Console.WriteLine("Açıklama: {0} --- Fiyat: {1}", car.Description, car.DailyPrice);
+            }
+
+            carService.Delete(car1);
+
+            Console.WriteLine("------------------------------");
+
+            foreach (var car in carService.GetAll())
+            {
+                Console.WriteLine("Açıklama: {0} --- Fiyat: {1}", car.Description, car.DailyPrice);
+            }
+
+            Console.WriteLine(carService.GetById(2).Id);
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,DailyPrice=90,ModelYear=2018,Description="Otomatik Dizel"},
+                new Car{Id=2,BrandId=1,ColorId=1,DailyPrice=160,ModelYear=2019,Description="Otomatik Hibrit"},
+                new Car{Id=3,BrandId=1,ColorId=1,DailyPrice=70,ModelYear=2012,Description="Manuel Benzinli"},
+                new Car{Id=4,BrandId=1,ColorId=1,DailyPrice=100,ModelYear=2016,Description="Otomatik Dizel"},
+                new Car{Id=5,BrandId=1,ColorId=1,DailyPrice=120,ModelYear=2019,Description="Otomatik Benzinli"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToUpdate);
+        }
+    }
+}

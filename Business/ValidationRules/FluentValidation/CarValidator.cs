fb0c3b264df113b3f55b25caa9472f6281ef7ed0
faruk <farukoonar@gmail.com>
2021-02-28 21:20:31 +0300

    AOP ve FluentValidation eklendi.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.ModelYear).Must(StartWith20);
+        }
+
+        private bool StartWith20(string arg)
+        {
+            return arg.StartsWith("20");
+        }
+    }
+}
